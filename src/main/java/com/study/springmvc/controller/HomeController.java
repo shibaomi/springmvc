d84efac5cca7372199bd2e0de0ca103267f2f6d0
shibaomi <shibaomi@shibaomi-PC>
2016-05-21 21:01:12 +0800

    s

--- src/main/java/com/study/springmvc/controller/HomeController.java
+++ src/main/java/com/study/springmvc/controller/HomeController.java
@@ -6,31 +6,41 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import com.study.springmvc.service.faces.TestService;
+
 @Controller
+@RequestMapping(value = "home/get")
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	private TestService testService;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "test", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		try {
+			logger.info("start");
+			testService.testService();
+			logger.info("end");
+		} catch (Exception e) {
+			logger.info("end1");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
