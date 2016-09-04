0743ba9d397d671a1cacaeca09adbc453fba63c1
shibaomi <shibaomi@shibaomi-PC>
2016-09-04 16:55:29 +0800

    菜单完善

--- src/main/webapp/views/authority/menumanage/js/menu.js
+++ src/main/webapp/views/authority/menumanage/js/menu.js
@@ -2,14 +2,28 @@
 var globalparam={
 		selectTreeNode:'',//树节点选择变量
 }
+//树插件初始化
+var setting = {
+	data: {
+		simpleData: {
+			enable: true,
+			id:"id",
+			pIdKey:"parentId"
+		},
+		key:{
+			children:"childMenu",
+			name:"menuName"
+		}
+	}
+};
 $(document).ready(function(){
 	$("form").validation({icon:true});	   
-	queryMenu();
 	//设置bootstrap弹出框属性--树菜单选择弹出框
 	$('#selectTreeMenuDIv').modal({
 		backdrop:'static',
 		show:false		
-	})
+	});
+	queryMenu();
 });
 //查询菜单标签
 function queryMenu(){
@@ -19,37 +33,31 @@ function queryMenu(){
 }
 //查询菜单成功回调函数
 function queryMenuSucc(data){
-	//树插件初始化
-	var setting = {
-		data: {
-			simpleData: {
-				enable: true,
-				id:"id",
-				pIdKey:"parentId"
-			},
-			key:{
-				children:"childMenu",
-				name:"menuName"
-			}
-		}
-	};
-	$.fn.zTree.init($("#menuTree"), setting, data.childMenu);
 	setting.callback={
 		onClick:clickTreeMenu,//选择树点击函数
 		onDblClick: dbClickTreeMenu//双击树节点选择菜单
 	};
-	$.fn.zTree.init($("#menuTree1"), setting, data.childMenu);
+	$.fn.zTree.init($("#menuTree"), setting, data.childMenu);
+	setting.callback={
+		onClick:clickSelectTreeMenu,//选择树点击函数
+		onDblClick: dbClickSelectTreeMenu//双击树节点选择菜单
+	};
+	$.fn.zTree.init($("#parentMenuTree"), setting, data.childMenu);
 }
 //单机树节点
-function clickTreeMenu(event, treeId, treeNode) {
+function clickSelectTreeMenu(event, treeId, treeNode) {
 	globalparam.selectTreeNode=treeNode;
 };
 //双击树节点
-function dbClickTreeMenu(event, treeId, treeNode) {
+function dbClickSelectTreeMenu(event, treeId, treeNode) {
 	//显示选择的父节点信息并关闭弹出框
+	if(isEmpty(treeNode)){
+		//表明双击树节点的其他地方了
+		return;
+	}
 	selectParentMenu(treeNode);
 };
-//
+//点击选择父菜单确认按钮
 function selectOkBtn(){
 	//显示选择的父节点信息并关闭弹出框
 	if(isEmpty(globalparam.selectTreeNode)){
@@ -57,30 +65,118 @@ function selectOkBtn(){
 		return;
 	}
 	selectParentMenu(globalparam.selectTreeNode);
+	globalparam.selectTreeNode="";//初始化选择的父节点信息
 }
-
+//显示选择的父节点信息
 function selectParentMenu(treeNode){
 	//显示选择的父节点信息
     $("#parentMenuDiv").val(treeNode.menuName);
     //设置选择的父节点id
     $("#parentMenuDiv").attr('parentId',treeNode.id);
+    $("#parentMenuDiv").attr('tId',treeNode.tId);
     //关闭菜单选择弹出框
     $('#selectTreeMenuDIv').modal('hide');
 }
-
-//点击保存或修改菜单按钮
-function savaOrUpdateMenu(){
-	if ($("form").valid(this,"error!")==false){
-		console.log("1111");
-	      return false;
-	    }else{
-			console.log("3333");
-	    }
+//取消选择的父节点信息
+function clearSelectparentMenu(){
+	$("#parentMenuDiv").val("");
+    //设置选择的父节点id
+    $("#parentMenuDiv").removeAttr('parentId');
+    $("#parentMenuDiv").removeAttr('tId');
 }
+//当选择父节点弹出时初始化选择的信息
+$('#selectTreeMenuDIv').on('show.bs.modal', function (e) {
+	var tId= $("#parentMenuDiv").attr('tId');
+	var treeObj = $.fn.zTree.getZTreeObj("parentMenuTree");
+	if(!isEmpty(tId)){
+		var node = treeObj.getNodeByTId(tId);
+		globalparam.selectTreeNode=node;
+		treeObj.selectNode(node);
+	}else{
+		//不选择任何节点
+		treeObj.selectNode("null");
+	}
+});
 
-function test(){
-	alertMsg("12313",{
-		okBtn:true,
-		okFunc:savaOrUpdateMenu
-	});
+//表格数据填写reset方法
+function formReset(){
+	$("#parentMenuDiv").removeAttr('parentId');
+    $("#parentMenuDiv").removeAttr('tId');
+}
+//点击选中的菜单进行查看
+function clickTreeMenu(event, treeId, treeNode){
+	//显示节点信息
+	showMenuInfo(treeNode);
+	//单击所有元素只能查看，不能修改
+	$("form").find("input").attr("disabled","disabled");
+	$("form").find("select").attr("disabled","disabled");
+	$("#clearParentMenuBtn").addClass("hide");
+	$(".glyphicon-search").addClass("hide");
+	$("#saveBtn").addClass("hide");
+	$("#resetBtn").addClass("hide");
+	$("#updateBtn").removeClass("hide");
+	$("#createBtn").removeClass("hide");
+}
+//双击选择的菜单进行修改
+function dbClickTreeMenu(event, treeId, treeNode){
+	showMenuInfo(treeNode);
+	setUpdatestate();
+}
+//点击新增按钮
+function createBtnFunc(){
+	$("form").find("input").removeAttr("disabled");
+	$("form").find("select").removeAttr("disabled");
+	$("#saveBtn").removeClass("hide");
+	$("#saveBtn").html("保存");
+	$("#resetBtn").removeClass("hide");
+	$("#updateBtn").addClass("hide");
+	$("#createBtn").addClass("hide");
+	$("#menuNameDiv").removeAttr('menuId');
+}
+//点击更新按钮
+function updateBtnFunc(){
+	setUpdatestate();
+}
+//修改状态设置
+function setUpdatestate(){
+	$("form").find("input").removeAttr("disabled");
+	$("form").find("select").removeAttr("disabled");
+	$("#clearParentMenuBtn").removeClass("hide");
+	$(".glyphicon-search").removeClass("hide");
+	$("#saveBtn").removeClass("hide");
+	$("#saveBtn").html("保存修改");
+	$("#resetBtn").addClass("hide");
+	$("#updateBtn").addClass("hide");
+	$("#createBtn").removeClass("hide");
+}
+//显示菜单信息
+function showMenuInfo(treeNode){
+	var parentNode=treeNode.getParentNode();
+	//菜单的id信息
+	$("#menuNameDiv").attr('menuId',treeNode.id);
+	//菜单所属系统
+	$("#ownSystemSelectDiv").val(treeNode.ownSystem);
+	//菜单名称
+	$("#menuNameDiv").val(treeNode.menuName);
+	//菜单状态
+	$("#menuStsSelectDiv").val(treeNode.sts);
+	//菜单类型
+	$("#menuTypeSelectDiv").val(treeNode.menuType);
+	//菜单URl
+	$("#menuUrlDiv").val(treeNode.menuUrl);
+	//菜单排序
+	$("#menuOrderDiv").val(treeNode.menuOrder);
+	if(!isEmpty(parentNode)){
+		//菜单的父菜单信息
+		$("#parentMenuDiv").val(parentNode.menuName);
+	    //设置选择的父节点id
+	    $("#parentMenuDiv").attr('parentId',parentNode.id);
+	    $("#parentMenuDiv").attr('tId',parentNode.tId);
+	}
+	//菜单图标类型
+	$("#menuIcoTypeSelectDiv").val(treeNode.icoType);
+	//菜单图标类或路径
+	$("#menuIcoMsgDiv").val(treeNode.icoMsg);
+	//述信息
+	$("#menuDescDiv").val(treeNode.menuDesc);
 }
--- src/main/webapp/views/common/common.js
+++ src/main/webapp/views/common/common.js
@@ -1,3 +1,80 @@
+var timeout=600000;//超时时间毫秒
+//Post方式Ajax方法
+function simplePostAjax(data,url,succCb){
+	$.ajax({
+		type:"POST",//请求类型，"POST" 或 "GET
+		cache:false,//默认: true,dataType为script和jsonp时默认为false，设置为 false 将不缓存此页面
+		url:url,//
+		data:data,//发送到服务器的数据
+		dataType:"json",//预期服务器返回的数据类型,"xml","html","script","json","jsonp","text"
+		ifModified:true,//(默认: false) 仅在服务器数据改变时获取新数据
+		timeout:timeout,//超时时间毫秒
+		//请求成功后的回调函数
+		success:function (data, textStatus, jqXHR){
+			if(succCb){
+				succCb(data, textStatus, jqXHR);
+			}
+		},
+		//请求失败时调用此函数。有以下三个参数：XMLHttpRequest 对象、错误信息、（可选）捕获的异常对象。
+		//如果发生了错误，错误信息（第二个参数）除了得到null之外，还可能是"timeout", "error", "notmodified" 和 "parsererror"
+		error:function (XMLHttpRequest, textStatus, errorThrown) {
+			if(textStatus=="timeout"){
+				alert("超时");
+			}else if(textStatus=="parsererror"){
+				alert("解析错误");
+			}else {
+				alert("fail:"+textStatus);
+			}
+		},
+		statusCode: {
+			404: function() {
+				alert('page not found');
+			}
+		},
+		complete:function (XMLHttpRequest, textStatus) {
+		    console.log("textStatus="+textStatus);
+		}
+	});
+}
+
+//get方式Ajax方法
+function simpleGetAjax(data,url,succCb){
+	$.ajax({
+		type:"GET",//请求类型，"POST" 或 "GET
+		cache:false,//默认: true,dataType为script和jsonp时默认为false，设置为 false 将不缓存此页面
+		url:url,//
+		data:data,//发送到服务器的数据
+		dataType:"json",//预期服务器返回的数据类型,"xml","html","script","json","jsonp","text"
+		ifModified:true,//(默认: false) 仅在服务器数据改变时获取新数据
+		timeout:timeout,//超时时间毫秒
+		//请求成功后的回调函数
+		success:function (data, textStatus, jqXHR){
+			if(succCb){
+				succCb(data, textStatus, jqXHR);
+			}
+		},
+		//请求失败时调用此函数。有以下三个参数：XMLHttpRequest 对象、错误信息、（可选）捕获的异常对象。
+		//如果发生了错误，错误信息（第二个参数）除了得到null之外，还可能是"timeout", "error", "notmodified" 和 "parsererror"
+		error:function (XMLHttpRequest, textStatus, errorThrown) {
+			if(textStatus=="timeout"){
+				alert("超时");
+			}else if(textStatus=="parsererror"){
+				alert("解析错误");
+			}else {
+				alert("fail:"+textStatus);
+			}
+		},
+		statusCode: {
+			404: function() {
+				alert('page not found');
+			}
+		},
+		complete:function (XMLHttpRequest, textStatus) {
+		    console.log("textStatus="+textStatus);
+		}
+	});
+}
+
 function isEmpty(data){
 	if(data==null||data==""||data==undefined){
 		return true;
